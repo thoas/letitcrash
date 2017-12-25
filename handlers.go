ed1c41a9b6ce5692fc2843a3ff1dc765a5ac3a01
Florent Messa <florent.messa@gmail.com>
2017-12-25 23:07:15 +0100

    Split defaultErrorHandler

---
+++ handlers.go
@@ -0,0 +1,57 @@
+package call911
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"reflect"
+	"runtime"
+	"strings"
+	"time"
+)
+
+// ErrorHandler is an handler to render the error.
+type ErrorHandler func(err interface{}, w http.ResponseWriter, r *http.Request)
+
+func defaultErrorHandler(err interface{}, w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+
+	queryString := make(map[string]string)
+
+	for key, value := range r.URL.Query() {
+		queryString[key] = fmt.Sprintf("%+v", value)
+	}
+
+	t := template.Must(template.New("debug").Parse(debugTpl))
+
+	headers := make(map[string]string)
+
+	for key, value := range r.Header {
+		if key == "Cookie" {
+			continue
+		}
+
+		headers[key] = fmt.Sprintf("%+v", value)
+	}
+
+	environments := make(map[string]string)
+
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		environments[pair[0]] = pair[1]
+	}
+
+	t.Execute(w, map[string]interface{}{
+		"queryString":       queryString,
+		"request":           r,
+		"error":             err,
+		"cookies":           r.Cookies(),
+		"version":           runtime.Version(),
+		"serverInformation": getServerInformation(),
+		"now":               time.Now(),
+		"errorType":         reflect.TypeOf(err).String(),
+		"headers":           headers,
+		"environments":      environments,
+	})
+}
